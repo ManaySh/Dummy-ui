7b2c2cfc405eaf43de5fa3a9639b3bfb94ba299d
Sudhakar Kadepaka <kadepakasudhakar8086@gmail.com>
2023-12-04 12:15:09 +0530

    routes update

--- src/Routes/MainRoutes.js
+++ src/Routes/MainRoutes.js
@@ -5,14 +5,11 @@ import SignUp from "../screens/SignUp";
 import ForgetPassword from "../screens/ForgetPassword"
 import UserList from "../Pages/UserList";
 import Buttons from '../Pages/buttons/index';
-<<<<<<< HEAD
 import Switches from '../Pages/switches';
 import Ratings from '../Pages/rating';
-=======
 import Cards from '../Pages/cards/index';
 
 
->>>>>>> 9f4d89e6fe7323b7bd7cba3f19c07845444c4136
 const MainRoutes = () => {
     return (
         <div>
@@ -26,12 +23,9 @@ const MainRoutes = () => {
                 <Route path="/forget-password" element={<ForgetPassword />} />
                 <Route path="/userList" element={<UserList />} />
                 <Route path="/buttons" element={<Buttons />} />
-<<<<<<< HEAD
                 <Route path="/switch" element={<Switches />} />
                 <Route path="/ratings" element={<Ratings />} />
-=======
                 <Route path="/cards" element={<Cards />} />
->>>>>>> 9f4d89e6fe7323b7bd7cba3f19c07845444c4136
             </Routes>
         </div>
     )
