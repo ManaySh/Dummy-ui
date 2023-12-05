afe96479a1ab75d76a0cfff89f8c8494093abda1
Sireesha <manay@innoclique.com>
2023-12-05 11:36:04 +0530

    for pull

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "../src/screens/Login";
-import SignUp from "../src/screens/SignUp";
-import ForgetPassword from "../src/screens/ForgetPassword";
-import Dashboard from "./components/Dashboard";
-import DialogInfo from "./Pages/DialogInfo";
-import Profile from "./Pages/DialogInfo";
 import Sidebar from "./components/Sidebar";
 function App() {
   return (
